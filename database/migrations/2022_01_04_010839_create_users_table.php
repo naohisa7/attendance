d8a78672006498095ae3e956f9aca3c3b03f9208
naohisa7 <guardkun1112@gmail.com>
2022-01-07 22:03:32 +0900

    first commit

---
+++ database/migrations/2022_01_04_010839_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary()->comment('ユーザーID');
+            $table->string('name', 255)->comment('名前');
+            $table->string('email', 255)->comment('メールアドレス');
+            $table->string('pass', 255)->comment('パスワード');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2022_01_04_010945_create_rests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rests', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary()->comment('休憩ID');
+            $table->unsignedBigInteger('user_id')->comment('ユーザーID');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->unsignedBigInteger('work_id')->comment('勤怠ID');
+            $table->foreign('work_id')->references('id')->on('works')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->time('start_time')->nullable()->comment('休憩開始時間');
+            $table->time('finish_time')->nullable()->comment('休憩終了時間');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rests');
+    }
+}
